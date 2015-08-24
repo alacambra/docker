28f682a82690aa97aa274d7f160185d6fa75f0a7
alacambra <alacambra@gmail.com>
2015-08-24 20:52:15 +0200

    first commit

---
+++ wildfly-base/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ${WILDFLY_HOME}
+/usr/sbin/sshd
+
+IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
+
+./bin/standalone.sh  -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR
