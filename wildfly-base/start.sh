9c35cd61656c3cc5bc39252b65664a1f38319a8b
alacambra <alacambra@gmail.com>
2015-08-25 23:24:42 +0200

    adding users

--- wildfly-base/start.sh
+++ wildfly-base/start.sh
@@ -4,4 +4,5 @@ cd ${WILDFLY_HOME}
 
 IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 
-./bin/standalone.sh  -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR
+#./bin/standalone.sh  -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR
+./bin/standalone.sh -b 0.0.0.0
